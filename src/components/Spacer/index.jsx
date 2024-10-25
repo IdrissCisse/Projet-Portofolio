01e9db5af0b8dc445ea8bcbb6668861b7c0efa60
Idriss Cisse <aliouc140@gmail.com>
2024-10-25 20:18:06 +0200

    Fixation du header lors du scroll

---
+++ src/components/Spacer/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Spacer = ({ size = '2rem' }) => <div style={{ height: size }} />;
+
+export default Spacer;
--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -4,17 +4,21 @@ import Header from '../../components/Header';
 import Home from '../../components/Home';
 import About from '../../components/About';
 import Projets from '../../components/Projets';
+import Spacer from '../../components/Spacer';
 
 function App() {
   return (
     <main>
       <Header />
+      <Spacer size="5rem" />
       <div id="home">
         <Home />
       </div>
+      <Spacer size="4rem" />
       <div id="about">
         <About />
       </div>
+      <Spacer size="15rem" />
       <div id="projects">
         <Projets />
       </div>
