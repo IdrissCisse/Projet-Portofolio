150f1d45b332697fac91a7f55cba58421941cce2
Idriss Cisse <aliouc140@gmail.com>
2024-10-30 23:25:43 +0100

    Implémentation de la section Projects

--- src/components/Projets/index.jsx
+++ src/components/Projets/index.jsx
@@ -1,46 +1,94 @@
+import './index.scss';
+import Card from '../../components/Card';
+import projectsData from '../../assets/accomodations.json';
+import webIcon from '../../assets/icons/web-svgrepo-com.svg';
+import githubIcon from '../../assets/icons/github-icon.svg';
+import slidesIcon from '../../assets/icons/slides-svgrepo-com.svg';
+
+import Tag from '../../components/Tag';
+
 const Projets = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-      laboriosam iste ipsam, numquam voluptatibus vitae corrupti. Illum
-      perferendis quidem voluptatum ipsum quo repellat fuga laudantium
-      aspernatur voluptas? Illo, voluptatibus consectetur.Contrairement à une
-      opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire.
-      Il trouve ses racines dans une oeuvre de la littérature latine classique
-      datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du
-      Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins
-      les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en
-      étudiant tous les usages de ce mot dans la littérature classique,
-      découvrit la source incontestable du Lorem Ipsum. Il provient en fait des
-      sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des
-      Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très
-      populaire pendant la Renaissance, est un traité sur la théorie de
-      l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit
-      amet...", proviennent de la section 1.10.32. L'extrait standard de Lorem
-      Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les
-      curieux. Les sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et
-      Malorum" de Cicéron sont aussi reproduites dans leur version originale,
-      accompagnée de la traduction anglaise de H. Rackham (1914).Lorem ipsum
-      dolor sit amet, consectetur adipisicing elit. Voluptatum laboriosam iste
-      ipsam, numquam voluptatibus vitae corrupti. Illum perferendis quidem
-      voluptatum ipsum quo repellat fuga laudantium aspernatur voluptas? Illo,
-      voluptatibus consectetur.Contrairement à une opinion répandue, le Lorem
-      Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans
-      une oeuvre de la littérature latine classique datant de 45 av. J.-C., le
-      rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en
-      Virginie, s'est intéressé à un des mots latins les plus obscurs,
-      consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les
-      usages de ce mot dans la littérature classique, découvrit la source
-      incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et
-      1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des
-      Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la
-      Renaissance, est un traité sur la théorie de l'éthique. Les premières
-      lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la
-      section 1.10.32. L'extrait standard de Lorem Ipsum utilisé depuis le XVIè
-      siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et
-      1.10.33 du "De Finibus Bonorum et Malorum" de Cicéron sont aussi
-      reproduites dans leur version originale, accompagnée de la traduction
-      anglaise de H. Rackham (1914).
+    <div className="projects">
+      <h2 className="projects-title">Projets</h2>
+      <div className="projects-content">
+        {projectsData.map((project, index) => (
+          <Card
+            key={index}
+            title={project.title}
+            image={project.image}
+            items={[
+              project.description,
+              <div
+                className="projects-content-skills-container"
+                style={{ marginTop: '1rem' }}
+              >
+                {project.skills.map((skill) => (
+                  <Tag key={skill} label={skill} />
+                ))}
+              </div>,
+            ]}
+            showActions={true}
+            actionContent={
+              <div
+                className="projects-content-actions"
+                style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}
+              >
+                {project.links.website && (
+                  <img
+                    src={webIcon}
+                    alt="Website"
+                    style={{
+                      cursor: 'pointer',
+                      width: '1.7rem',
+                      height: '1.7rem',
+                      marginRight: '0.7rem',
+                    }}
+                    onClick={() => window.open(project.links.website, '_blank')}
+                  />
+                )}
+                {project.links.github && (
+                  <img
+                    src={githubIcon}
+                    alt="GitHub"
+                    style={{
+                      cursor: 'pointer',
+                      width: '1.7rem',
+                      height: '1.7rem',
+                    }}
+                    onClick={() => window.open(project.links.github, '_blank')}
+                  />
+                )}
+                {project.links.drive && (
+                  <img
+                    src={slidesIcon}
+                    alt="Google Slides"
+                    style={{
+                      cursor: 'pointer',
+                      width: '1.8rem',
+                      height: '1.8rem',
+                    }}
+                    onClick={() => window.open(project.links.drive, '_blank')}
+                  />
+                )}
+              </div>
+            }
+            onActionClick={() =>
+              window.open(
+                project.links.github || project.links.website,
+                '_blank'
+              )
+            }
+            bgColor="#f7f9fb"
+            textColor="#010816"
+            cardWidth="100%"
+            cardHeight={`${20}rem`}
+            titleSize="1.6rem"
+            textSize="1rem"
+            itemsHeight="fit-content"
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.scss';
+const Tag = ({ label }) => {
+  return <span className="skills-tag">{label}</span>;
+};
+
+export default Tag;
