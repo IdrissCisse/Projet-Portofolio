d8f5711d120e59f6e61feae162b9f66f4df7783f
Idriss Cisse <aliouc140@gmail.com>
2024-11-06 11:24:05 +0100

    Ajout de la basename dans Router

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from '../../pages/App';
 
 const AppRouter = () => (
-  <Router>
+  <Router basename="/Projet-Portofolio">
     <Routes>
       <Route path="/" element={<App />} />
     </Routes>
